07de1fe85c77ce90b0e7d6fd06b346793265b03f
tppppp <tppppp4free@gmail.com>
2017-03-07 22:46:22 +0800

    whole project with maven

---
+++ sams/src/main/java/cn/tp/SamsApplication.java
@@ -0,0 +1,12 @@
+package cn.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsApplication.class, args);
+	}
+}
