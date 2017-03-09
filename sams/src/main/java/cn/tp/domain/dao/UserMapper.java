836006ec293eff9fcaf9ce5abacf0220b2bb708c
tppppp <tppppp4free@gmail.com>
2017-03-09 23:15:28 +0800

    add some code

---
+++ sams/src/main/java/cn/tp/domain/dao/UserMapper.java
@@ -0,0 +1,16 @@
+package cn.tp.domain.dao;
+
+import cn.tp.domain.pojo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by tppppp on 2017/3/9.
+ */
+@Mapper
+public interface UserMapper {
+
+    @Select("select * from user where name = #{name}")
+    User findUserByName(@Param("name")String name);
+}
