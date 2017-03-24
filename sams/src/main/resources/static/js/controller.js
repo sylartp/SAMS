f39ffa80c29fa01c61c44acf6bae3f909719a5de
tppppp <tppppp4free@gmail.com>
2017-03-24 22:57:50 +0800

    add some

--- sams/src/main/resources/static/js/controller.js
+++ sams/src/main/resources/static/js/controller.js
@@ -2,18 +2,24 @@
  * Created by tppppp on 2017/3/16.
  */
 indexApp
-    .controller("indexCtrl",function($scope, $route, $routeParams, $location){
-    $scope.$route = $route;
-    $scope.$location = $location;
-    $scope.$routeParams = $routeParams;
+    .controller("indexCtrl", function ($scope, $route, $routeParams, $location) {
+        $scope.$route = $route;
+        $scope.$location = $location;
+        $scope.$routeParams = $routeParams;
     })
-    .controller("login",function($scope,$http){
-        $scope.submit = function(path){
+    .controller("login", function ($scope, $http) {
+        $scope.submit = function (path) {
 
-            window.location = 'views/'+path+'.html';
+            window.location = 'views/' + path + '.html';
         }
     })
-    .controller("register",function ($scope,$http){
+    .controller("register", function ($scope, $http) {
+        $scope.judgement = false;
+        var pwd1 = $scope.pwd1;
+        var pwd2 = $scope.pwd2;
+        if (pwd1 !== pwd2) {
+            $scope.judgement = true;
+        }
 
     });
 
