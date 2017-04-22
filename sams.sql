cab0b25d9fcaea93c395c1ccd4bda23c4de92445
tppppp <tppppp4free@gmail.com>
2017-04-22 10:56:16 +0800

    Add files via upload
    sql file

---
+++ sams.sql
@@ -0,0 +1,141 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for osx10.12 (x86_64)
+--
+-- Host: localhost    Database: sams
+-- ------------------------------------------------------
+-- Server version	5.7.17
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Club`
+--
+
+DROP TABLE IF EXISTS `Club`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Club` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `content` varchar(100) DEFAULT NULL,
+  `present_number` int(11) DEFAULT NULL,
+  `max_number` int(11) DEFAULT '50',
+  `image` varchar(100) DEFAULT NULL,
+  `create_time` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Club`
+--
+
+LOCK TABLES `Club` WRITE;
+/*!40000 ALTER TABLE `Club` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Club` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Major`
+--
+
+DROP TABLE IF EXISTS `Major`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Major` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Major`
+--
+
+LOCK TABLES `Major` WRITE;
+/*!40000 ALTER TABLE `Major` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Major` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Role`
+--
+
+DROP TABLE IF EXISTS `Role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Role` (
+  `id` int(5) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Role`
+--
+
+LOCK TABLES `Role` WRITE;
+/*!40000 ALTER TABLE `Role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(128) DEFAULT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `gender` int(2) DEFAULT '1',
+  `birthday` date DEFAULT NULL,
+  `hobby` varchar(255) DEFAULT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `role_id` int(5) DEFAULT NULL,
+  `major_id` int(10) DEFAULT NULL,
+  `club_id` int(5) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  KEY `fk_User_Role` (`role_id`),
+  KEY `fk_User_Major` (`major_id`),
+  KEY `fk_User_Club` (`club_id`),
+  CONSTRAINT `fk_User_Club` FOREIGN KEY (`club_id`) REFERENCES `Club` (`id`),
+  CONSTRAINT `fk_User_Major` FOREIGN KEY (`major_id`) REFERENCES `Major` (`id`),
+  CONSTRAINT `fk_User_Role` FOREIGN KEY (`role_id`) REFERENCES `Role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `User`
+--
+
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (11,'tppppp4free@gmail.com','123456','123',1,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-04-22 10:52:45
